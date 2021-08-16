202b0248a44294703ee68dda4f9f0106c98ac8f3
Tich <67440052+t1ch@users.noreply.github.com>
2021-08-16 08:37:26 +0200

    Update crc.lisp

--- crc.lisp
+++ crc.lisp
@@ -3,15 +3,16 @@
 (defun degree-of-polynomial (polynomial)
   (1- (length polynomial)))
 
-(defun initialize-crc-register (degree-of-polynomial)
-  (loop for i from 1 upto degree-of-polynomial
+(defun list-of-zeros (number-of-zeros)
+  (loop for i from 1 upto number-of-zeros
 	collect
 	0))
 
+(defun initialize-crc-register (degree-of-polynomial)
+  (list-of-zeros degree-of-polynomial))
+
 (defun augment-message (degree-of-polynomial message)
-  (append message (loop for i from 1 upto degree-of-polynomial
-			collect
-			0)))
+  (append message (list-of-zeros degree-of-polynomial)))
 
 (defun most-significant-bit (crc-register)
   (car crc-register))
@@ -20,11 +21,8 @@
   (append (rest register) (list (car message))))
 
 (defun xor (list-1 list-2)
-  (loop for x in list-1
-	for y in list-2
-	collect
-	(logxor x y)))
-
+  (map 'list #'(lambda (element-1 element-2) (logxor element-1 element-2)) list-1 list-2))
+  
 (defun crc (message crc-register polynomial)
   (if (null message)
       crc-register 
@@ -36,9 +34,25 @@
 	    (crc (rest message) xored-register polynomial)
 	    (crc (rest message) updated-crc-register polynomial)))))
 
+(defun crc-1 (message crc-register polynomial)
+  (if (= (length message) 1)
+      crc-register 
+      (let* ((crc-register-msb (most-significant-bit crc-register))
+	     (updated-crc-register (update-crc-register crc-register message))
+	     (xored-register (xor updated-crc-register (rest polynomial))))
+	(format t "~s~%" crc-register)
+	(if (= 1 crc-register-msb)
+	    (crc-1 (rest message) xored-register polynomial)
+	    (crc-1 (rest message) updated-crc-register polynomial)))))
 
 (defun simple-crc (polynomial message)
   (let* ((degree-of-polynomial (degree-of-polynomial polynomial))
 	 (crc-register (initialize-crc-register degree-of-polynomial))
 	 (augmented-message (augment-message degree-of-polynomial message)))
     (crc augmented-message crc-register polynomial)))
+
+(defun simple-crc-1 (polynomial message)
+  (let* ((degree-of-polynomial (degree-of-polynomial polynomial))
+	 (crc-register (initialize-crc-register degree-of-polynomial))
+	 (augmented-message (augment-message degree-of-polynomial message)))
+    (crc-1 augmented-message crc-register polynomial)))
