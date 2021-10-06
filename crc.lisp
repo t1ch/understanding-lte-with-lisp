0221dffd53752a23b4ca8ba61cca0a56f586fa84
Tich <67440052+t1ch@users.noreply.github.com>
2021-10-06 21:11:48 +0200

    Update crc.lisp

--- crc.lisp
+++ crc.lisp
@@ -1,5 +1,6 @@
 ;;; A simple implementation of serial CRC based on the paper "A PAINLESS GUIDE TO CRC ERROR DETECTION ALGORITHMS" by Ross N. Williams
 ;;; The Polynomial and message are passed as lists e.g (simple-crc '(1 0 0 1 0 1) '(1 0 0 1)) 
+;;; The code then progresses into a parallel crc generator based on the paper "A Practical Parallel CRC Generation Method" by Evgeni Stavinov http://outputlogic.com/?p=158
 (defun degree-of-polynomial (polynomial)
   (1- (length polynomial)))
 
@@ -56,3 +57,21 @@
 	 (crc-register (initialize-crc-register degree-of-polynomial))
 	 (augmented-message (augment-message degree-of-polynomial message)))
     (crc-1 augmented-message crc-register polynomial)))
+(defun list-shift-left (list number-of-shifts)
+  (if (< number-of-shifts 1)
+      list
+      (list-shift-left (append (rest list) (list (car list))) (1- number-of-shifts))))
+
+
+(defun generate-one-hot-lists (bit-width)
+  (let ((seed (append (list-of-zeros (1- bit-width)) (list 1))))
+   (loop for i from 0 upto (1- bit-width)  
+		  collect
+		  (list-shift-left seed i))))
+
+(defun generate-NxM-matrix (polynomial message-width)
+  (map 'list #'(lambda (x) (simple-crc polynomial x)) (generate-one-hot-lists message-width)))
+
+(defun generate-MxM-matrix (polynomial)
+  (let ((polynomial-width (degree-of-polynomial polynomial)))
+    (map 'list #'(lambda (x) (simple-crc-1 polynomial x)) (generate-one-hot-lists polynomial-width))))
